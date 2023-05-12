f951ebe62142c3f70cfe0c7d8aaaefd2381f27a8
Rajan <65783031+Rajan03@users.noreply.github.com>
2023-05-12 22:39:14 +0530

    refactor: fixed layout of about section

---
+++ pages/about.tsx
@@ -0,0 +1,38 @@
+import Image from 'next/image';
+
+const About = () => {
+  return (
+    <div className="relative w-[100%] h-1/2 flex justify-end">
+      <Image
+        className="object-cover min-w-[70%] h-full object-top"
+        height={500}
+        width={500}
+        alt="Picture of the author"
+        src="https://img.freepik.com/free-photo/group-people-working-out-business-plan-office_1303-15874.jpg?"
+      />
+
+      <div
+        className="absolute w-[60%] top-0 left-0 h-full 
+        bg-gradient-to-r from-black to-gray-900 rounded-br-full rounded-tr-[20%]
+        flex justify-center items-center"
+      >
+        <div className="flex flex-col justify-center items-end gap-y-6">
+          <h1 className="text-3xl font-medium flex justify-center items-center gap-x-8">
+            <span className="text-white">HOME</span>
+            <span className="text-red-500"> | </span>
+            <span className="text-red-500"> COMPANY ABOUT</span>
+          </h1>
+          <h2 className="text-7xl text-white ">
+            <strong>COMPANY ABOUT</strong>
+          </h2>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default About;
+
+/*
+ 
+*/
