ca21ce98c7a251a190e0c2e8fb6eca213bdb6205
Samiksha <ssamiksha271@gmail.com>
2023-06-04 16:55:55 +0530

    feat: working on about

--- components/ContactCard.tsx
+++ components/ContactCard.tsx
@@ -1,23 +1,75 @@
-import React from "react";
-import {IconType} from "react-icons";
+import { QUOTE_BG } from '@/utils/constant';
+import React, { useState } from 'react';
+import { IconType } from 'react-icons';
 
 interface ContactCardProps {
-    title: string
-    subtitle: string
-    text1: string
-    text2: string
-    Icon: IconType;
-
-    isLink: boolean
+  title: string;
+  subtitle: string;
+  text1: string;
+  text2: string;
+  Icon: IconType;
+  isLink: boolean;
 }
 
-const ContactCard: React.FC<ContactCardProps> = ({Icon, isLink, text2, text1, subtitle, title}) => {
+const ContactCard: React.FC<ContactCardProps> = ({
+  Icon,
+  isLink,
+  text2,
+  text1,
+  subtitle,
+  title,
+}) => {
+  const [isHovered, setIsHovered] = useState(false);
 
-    return (
-        <>
-            <Icon/>
-        </>
-    )
-}
+  const handleMouseEnter = () => {
+    setIsHovered(true);
+  };
 
-export default ContactCard
+  const handleMouseLeave = () => {
+    setIsHovered(false);
+  };
+
+  const cardStyle = {
+    backgroundColor: isHovered ? 'black' : 'white',
+    backgroundImage: isHovered ? `url(${QUOTE_BG})` : '',
+    backgroundRepeat: 'no-repeat',
+    backgroundSize: 'cover',
+    transition: 'background-color 0.3s',
+  };
+
+  const textStyle = {
+    color: isHovered ? 'white' : '#000',
+    transition: 'color 0.3s',
+  };
+
+  return (
+    <div className="col-span-1 md:col-span-1 lg:col-span-1">
+      <div className="relative">
+        <div
+          className="bg-white rounded-lg shadow-lg py-[30%] px-10"
+          onMouseEnter={handleMouseEnter}
+          onMouseLeave={handleMouseLeave}
+          style={cardStyle}
+        >
+          <h1 className="text-lg md:text-4xl font-bold mb-4  text-primary-600">
+            <span style={textStyle}>{title}</span>
+          </h1>
+          <h2 className="text-3xl font-medium text-dark mb-20">
+            <span style={textStyle}>{subtitle}</span>
+          </h2>
+          <p className="text-xl font-normal text-justify text-dark">
+            <span style={textStyle}>{text1}</span>
+          </p>
+          <p className="text-xl font-normal text-justify text-dark ">
+            <span style={textStyle}>{text2}</span>
+          </p>
+        </div>
+        <div className="absolute top-0 left-20 transform -translate-x-1/2 -translate-y-1/2">
+          <Icon className="text-[100px] ml-10 text-primary-600" />
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default ContactCard;
--- modules/contact/CardsSection.tsx
+++ modules/contact/CardsSection.tsx
@@ -1,15 +1,35 @@
-import {ContactCard} from "@/components";
-import {MdEmail} from "react-icons/md";
-
+import { ContactCard } from '@/components';
+import { MdCall, MdEmail, MdLocationOn } from 'react-icons/md';
 
 const CardsSection = () => {
-    return (
-        <>
-            <ContactCard Icon={MdEmail} isLink subtitle={''} text1={''} text2={''} title={''}/>
-            <ContactCard Icon={MdEmail} isLink subtitle={''} text1={''} text2={''} title={''}/>
-            <ContactCard Icon={MdEmail} isLink subtitle={''} text1={''} text2={''} title={''}/>
-        </>
-    )
-}
+  return (
+    <div className=" mt-[10%] container mx-auto grid gap-10 grid-cols-3">
+      <ContactCard
+        Icon={MdLocationOn}
+        isLink
+        subtitle={'You Are Most Welcome To Visit Office.'}
+        text1={'8502 preston rd. inglewood,maine 98380'}
+        text2={'   '}
+        title={'Office Location'}
+      />
+      <ContactCard
+        Icon={MdCall}
+        isLink
+        subtitle={'Keeping You Always Better Connected        '}
+        text1={'+125 (874) 452 645'}
+        text2={'+125 (895) 658 568'}
+        title={'Talk With US'}
+      />
+      <ContactCard
+        Icon={MdEmail}
+        isLink
+        subtitle={'Drop Us A Mail We Will Answer You Asap        '}
+        text1={'info.conbix@gmail.com'}
+        text2={'conbix@gmail.com'}
+        title={'Quick Email        '}
+      />
+    </div>
+  );
+};
 
-export default CardsSection
+export default CardsSection;
