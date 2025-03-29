d13c4a7b5a613935cbecad18c674d09c77e344ec
Rajan Verma <rajanverma5410@gmail.com>
2025-03-29 17:57:51 +0530

    minor updates for working app

--- modules/home/About.tsx
+++ modules/home/About.tsx
@@ -1,59 +1,75 @@
-import {AnimatedBtn, SectionHeader} from '@/components';
-import {SanityAboutHome} from '@/types';
+import { AnimatedBtn, SectionHeader } from '@/components';
+import { SanityAboutHome } from '@/types';
 import Image from 'next/image';
 import React from 'react';
 
-const About: React.FC<SanityAboutHome> = ({title, subtitle, description, count, images}) => {
-    return (
-        <>
-            <div className="min-h-full container mx-auto gap-y-16 gap-x-8 flex flex-col lg:flex-row py-8 sm:py-12">
-                {/* Image Section */}
-                <div className="relative min-h-[inherit] flex-[65%] flex justify-start items-stretch gap-x-12 sm:py-20">
-                    <div className="relative min-h-[50vh] w-full sm:w-1/2">
-                        <Image src={images[0]} alt="" fill
-                               className="rounded select-none object-cover object-right w-full h-full"
-                        />
-                    </div>
+const About: React.FC<SanityAboutHome> = ({
+  title,
+  subtitle,
+  description,
+  count,
+  images,
+}) => {
+  return (
+    <>
+      <div className="min-h-full container mx-auto gap-y-16 gap-x-8 flex flex-col lg:flex-row py-8 sm:py-12">
+        {/* Image Section */}
+        <div className="relative min-h-[inherit] flex-[65%] flex justify-start items-stretch gap-x-12 sm:py-20">
+          <div className="relative min-h-[50vh] w-full sm:w-1/2">
+            <Image
+              src={images ? images[0] : ''}
+              alt=""
+              fill
+              className="rounded select-none object-cover object-right w-full h-full"
+            />
+          </div>
 
-                    <div className="relative hidden sm:inline-flex w-1/2 min-h-[50vh]">
-                        <Image src={images[1]} alt="" fill
-                               className="rounded select-none object-cover object-right w-full h-full"
-                        />
-                    </div>
+          <div className="relative hidden sm:inline-flex w-1/2 min-h-[50vh]">
+            <Image
+              src={images ? images[1] : ''}
+              alt=""
+              fill
+              className="rounded select-none object-cover object-right w-full h-full"
+            />
+          </div>
 
-                    {count && <div
-                        className="hidden vsm:flex absolute sm:bottom-12 sm:left-[10%] h-[28rem] w-[22rem] bg-primary-600
+          {count && (
+            <div
+              className="hidden vsm:flex absolute sm:bottom-12 sm:left-[10%] h-[28rem] w-[22rem] bg-primary-600
             text-white flex-col justify-start items-center p-6 pt-12"
-                        style={{clipPath: 'polygon(0 0, 100% 0, 100% 76%, 0% 100%)'}}
-                    >
-                        <p className="text-7xl uppercase font-bold text-start ">{count}</p>
-                        <span className="h-[1px] w-1/2 bg-neutral-100 mb-6"/>
-                        <p className="text-2xl sm:text-3xl leading-[2.5rem] font-normal text-center">
-                            Years Experience On Business Consulting
-                        </p>
-                    </div>}
-                </div>
-
-                {/* Text Section */}
-                <div className="flex-[45%] flex flex-col justify-start lg:px-8 items-start lg:justify-center">
-                    <SectionHeader showLine title={subtitle} className="mb-8"/>
-                    <div className="text-6xl font-bold text-dark leading-sm text-start">
-                        {title}
-                    </div>
-                    {description && (
-                        <p className="text-xl text-dark mt-12 max-w-[80%] text-start">
-                            {description}
-                        </p>
-                    )}
-                    <AnimatedBtn
-                        text="Discover More"
-                        className="mt-12 text-xl"
-                        link="/about#services"
-                    />
-                </div>
+              style={{ clipPath: 'polygon(0 0, 100% 0, 100% 76%, 0% 100%)' }}
+            >
+              <p className="text-7xl uppercase font-bold text-start ">
+                {count}
+              </p>
+              <span className="h-[1px] w-1/2 bg-neutral-100 mb-6" />
+              <p className="text-2xl sm:text-3xl leading-[2.5rem] font-normal text-center">
+                Years Experience On Business Consulting
+              </p>
             </div>
-        </>
-    );
+          )}
+        </div>
+
+        {/* Text Section */}
+        <div className="flex-[45%] flex flex-col justify-start lg:px-8 items-start lg:justify-center">
+          <SectionHeader showLine title={subtitle} className="mb-8" />
+          <div className="text-6xl font-bold text-dark leading-sm text-start">
+            {title}
+          </div>
+          {description && (
+            <p className="text-xl text-dark mt-12 max-w-[80%] text-start">
+              {description}
+            </p>
+          )}
+          <AnimatedBtn
+            text="Discover More"
+            className="mt-12 text-xl"
+            link="/about#services"
+          />
+        </div>
+      </div>
+    </>
+  );
 };
 
 export default About;
--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,71 +1,72 @@
-import {ErrorBoundary, Footer, Navbar} from '@/components';
+import { ErrorBoundary, Footer, Navbar } from '@/components';
+import { useNavState } from '@/hooks/navState';
+import { ContactModal } from '@/modules/contact';
+import { ResourceModal } from '@/modules/resources';
+import { TeamModal } from '@/modules/teams';
+import Error from '@/pages/_error';
 import '@/styles/globals.css';
-import type {AppProps} from 'next/app';
-import {Montserrat} from 'next/font/google';
-import {useRouter} from "next/router";
-import {useEffect, useState} from "react";
-import Error from "@/pages/_error";
-import {TeamModal} from "@/modules/teams";
-import {ContactModal} from "@/modules/contact";
-import {ResourceModal} from "@/modules/resources";
-import {Toaster} from "react-hot-toast";
-import {useNavState} from "@/hooks/navState";
+import type { AppProps } from 'next/app';
+import { Montserrat } from 'next/font/google';
+import { useRouter } from 'next/router';
+import { useEffect, useState } from 'react';
+import { Toaster } from 'react-hot-toast';
 
 // Font import
 const font = Montserrat({
-    subsets: ['latin'],
-    weight: ['300', '400', '500', '600', '700', '800', '900'],
+  subsets: ['latin'],
+  weight: ['300', '400', '500', '600', '700', '800', '900'],
 });
 
-const Loading = () => <div
-    className="h-1 w-full bg-primary-600 fixed top-0 left-0 z-50 animate-width duration-700 ease-linear"></div>
+const Loading = () => (
+  <div className="h-1 w-full bg-primary-600 fixed top-0 left-0 z-50 animate-width duration-700 ease-linear"></div>
+);
 
-export default function App({Component, pageProps}: AppProps) {
-    const router = useRouter();
-    const [isLoading, setIsLoading] = useState(false);
-    const {setIsOpen} = useNavState();
+export default function App({ Component, pageProps }: AppProps) {
+  const router = useRouter();
+  const [isLoading, setIsLoading] = useState(false);
+  const { setIsOpen } = useNavState();
 
-    useEffect(() => {
-        const handleStart = () => {
-            setIsLoading(true);
-        }
-        const handleComplete = () => {
-            setIsOpen(false);
-            setIsLoading(false);
-        }
+  useEffect(() => {
+    const handleStart = () => {
+      setIsLoading(true);
+    };
+    const handleComplete = () => {
+      setIsOpen(false);
+      setIsLoading(false);
+    };
 
-        router.events.on('routeChangeStart', handleStart);
-        router.events.on('routeChangeComplete', handleComplete);
-        router.events.on('routeChangeError', handleComplete);
+    router.events.on('routeChangeStart', handleStart);
+    router.events.on('routeChangeComplete', handleComplete);
+    router.events.on('routeChangeError', handleComplete);
 
-        return () => {
-            router.events.off('routeChangeStart', handleStart);
-            router.events.off('routeChangeComplete', handleComplete);
-            router.events.off('routeChangeError', handleComplete);
-        }
-    }, [router]);
+    return () => {
+      router.events.off('routeChangeStart', handleStart);
+      router.events.off('routeChangeComplete', handleComplete);
+      router.events.off('routeChangeError', handleComplete);
+    };
+  }, [router]);
 
-    return (
-        <>
-            <Toaster position={'top-right'}/>
-            <TeamModal/>
-            <ContactModal/>
-            <ResourceModal/>
+  return (
+    <>
+      <Toaster position={'top-right'} />
+      <TeamModal />
+      <ContactModal />
+      <ResourceModal />
 
-            <div className={'min-h-screen w-full ' + font.className}>
-                <Navbar/>
+      <div className={'min-h-screen w-full ' + font.className}>
+        <Navbar />
 
-                {/* Loading Spinner */}
-                {isLoading && <Loading/>}
+        {/* Loading Spinner */}
+        {isLoading && <Loading />}
 
-                {/* Page Content */}
-                <ErrorBoundary fallback={<Error/>}>
-                    <Component {...pageProps} />
-                </ErrorBoundary>
+        {/* Page Content */}
+        <ErrorBoundary fallback={<Error />}>
+          <Component {...pageProps} />
+        </ErrorBoundary>
 
-                {/* Footer */}
-                <Footer/>
-            </div>
-        </>
-    );
+        {/* Footer */}
+        <Footer />
+      </div>
+    </>
+  );
 }
